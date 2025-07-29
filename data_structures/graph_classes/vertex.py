bfe60f01b52f8e8aa79c6f89d547b8a3fda83ca4
Jotham Hughes <102107806@student.swin.edu.au>
2025-07-29 14:34:54 +1000

    The edges atribute of the vertex now holds a singlyLinkedList and can have edges added to this list

--- data_structures/graph_classes/vertex.py
+++ data_structures/graph_classes/vertex.py
@@ -1,6 +1,11 @@
+from linked_list.singly_linked_list import SinglyLinkedList
+
 class Vertex:
     def __init__(self, id, x, y):
         self.id = id # The id (number of the vertex)
         self.x = x # The x coordinates of the vertex
         self.y = y # The y coordinates of the vertex
-        self.edges = None # A linked list that will store all the id's of the vertexes edges
+        self._edges = SinglyLinkedList() # A linked list that will store all the id's of the vertexes edges
+
+    def add_edge(self, edge):
+        self._edges.append(edge) # Append an edge to the singularly linked list

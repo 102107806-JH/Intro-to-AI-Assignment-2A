4350055cc80b7e12445b1aca3774ccef108e5cab
Jotham Hughes <102107806@student.swin.edu.au>
2025-07-29 15:17:37 +1000

    Main changes for testing

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from data_structures.graph_classes.linked_list.singly_linked_list import SinglyLinkedList
+from graph_init.graph_init import GraphInitialization
 
-testList = SinglyLinkedList()
-
-for i in range(11):
-    print(i)
-    testList.append(i)
-
-cur_item = testList._head
-while cur_item is not None:
-    print(cur_item.data_getter())
-    cur_item = cur_item.next_getter()
-
-print("end")
+graphInit = GraphInitialization(r"data/PathFinder-test.txt")
+graphInit.get_adjacency_list_graph()

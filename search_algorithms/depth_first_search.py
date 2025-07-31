b7eb3906bd70f727a028285b93c2c0a8819d7ea8
Jotham Hughes <102107806@student.swin.edu.au>
2025-08-01 01:55:42 +1000

    Changing the dictionary name in dfs to 'Reached' to allign with other algorithms

--- search_algorithms/depth_first_search.py
+++ search_algorithms/depth_first_search.py
@@ -6,7 +6,7 @@ from search_algorithms.helper_functions.node_depth import node_depth
 def depth_first_search(problem, depth_limit):
     frontier = Stack()  # Frontier stack #
     frontier.push(Node(state=problem.initial_state))  # Push the initial node onto the stack #
-    observed_states = {problem.initial_state : True}  # Dictionary for checking cycles in this problem the same state should never be encountered again #
+    reached = {problem.initial_state : True}  # Dictionary for checking cycles in this problem the same state should never be encountered again #
 
     while frontier.is_empty() == False:  # Keep going until the frontier is empty #
 
@@ -22,9 +22,9 @@ def depth_first_search(problem, depth_limit):
                 # ascending order because stack is used need to push higher nodes first thus list needs reversing by state #
 
                 # Only push the child if that state has never been on the frontier before
-                if observed_states.get(child.state, False) == False:
+                if reached.get(child.state, False) == False:
                     frontier.push(child)  # Push child onto the frontier #
-                    observed_states[child.state] = True  # The pushed child has been on the frontier so indicate this in the dictionary #
+                    reached[child.state] = True  # The pushed child has been on the frontier so indicate this in the dictionary #
 
     return None  # No solution has been found #
 

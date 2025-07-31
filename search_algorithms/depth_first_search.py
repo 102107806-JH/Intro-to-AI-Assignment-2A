8b60a7902aef9c47ebc8d5d0f8f57b6fce1c75ce
Jotham Hughes <102107806@student.swin.edu.au>
2025-08-01 01:53:45 +1000

    Removing cost calculations from dfs expand as they are not needed

--- search_algorithms/depth_first_search.py
+++ search_algorithms/depth_first_search.py
@@ -36,8 +36,7 @@ def expand(problem, node):
     for action in problem.actions(state):  # Go through all the states #
 
         new_state = problem.result(state, action)  # Get the state that results from an action #
-        cost = node.path_cost + problem.action_cost(state, action, new_state)  # Get the path cost to the current node #
-        children.append(Node(state=new_state, parent=node, action=action, path_cost=cost))  # Append a new child node using the node constructor #
+        children.append(Node(state=new_state, parent=node, action=action))  # Append a new child node using the node constructor #
 
     return children
 

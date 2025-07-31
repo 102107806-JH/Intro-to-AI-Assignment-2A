b05f99f558cd55a158bda0f1cf4802ebf8b8b0d6
Jotham Hughes <102107806@student.swin.edu.au>
2025-07-31 16:46:15 +1000

    Made a start on gbfs. The following things still need to be done: 1)The algorithm needs to be finished a method needs to be added to the problem that gets the distance from a state to the closest goal state 2) Once the branch is re-merged expand needs to be brought out of its own folder and under the specific algorithm file because it varies between searching algorithms 3) May also need toadd a method and attribute to tell in which order the node has been added to the queue

---
+++ search_algorithms/greedy_best_first_search.py
@@ -0,0 +1,36 @@
+from data_structures.queues.priority_que_JH import PriorityQueue
+from textbook_abstractions.node import Node
+
+def greed_best_first_search(problem):
+    node = Node(state=problem.initial_state)
+    frontier = PriorityQueue(key_lambda=lambda node:(node.total_cost, node.state))
+    frontier.push(node)
+    reached = {problem.initial_state : True}
+
+    while frontier.is_empty() == False:
+
+        node = frontier.pop()
+
+        if problem.is_goal(node.state):
+            return node
+
+        for child in expand(problem, node):
+            state = child.state
+
+            if reached.get(state, False) == False:
+                reached[state] = True
+                frontier.push(child)
+
+    return None
+
+def expand(problem, node):
+
+    state = node.state  # The state of the parent node #
+    children = []  # List to store all the child nodes #
+
+    for action in problem.actions(state):  # Go through all the states #
+
+        new_state = problem.result(state, action)  # Get the state that results from an action #
+        children.append(Node(state=new_state, parent=node, action=action, path_cost=0, heuristic_cost=))  # Append a new child node using the node constructor #
+
+    return children

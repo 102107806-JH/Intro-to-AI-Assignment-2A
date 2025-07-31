632f96525e2846d65df41a7bd07f1dada9c3384f
Jotham Hughes <102107806@student.swin.edu.au>
2025-08-01 02:46:17 +1000

    Commenting is_cycle function

--- search_algorithms/helper_functions/cycle_checker.py
+++ search_algorithms/helper_functions/cycle_checker.py
@@ -1,13 +1,13 @@
 def is_cycle(node, cycle_depth_limit):
-    current_node = node.parent
+    current_node = node.parent  # Go one node up in the tree #
 
-    while cycle_depth_limit > 0 and current_node is not None:
+    while cycle_depth_limit > 0 and current_node is not None:  # Keep going up nodes in the tree until the cycle depth limit is reached or you run out of nodes
 
-        if node.state == current_node.state:
+        if node.state == current_node.state:  # A cycle has been found #
             return True
 
-        cycle_depth_limit -= 1
-        current_node = current_node.parent
+        cycle_depth_limit -= 1  # Decrement depth limit #
+        current_node = current_node.parent  # Move up the tree #
 
 
 
